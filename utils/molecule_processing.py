042eb965280df5d6b74ec621b385e37175b73289
Stas K <stas.kurass@gmail.com>
2026-01-25 15:13:35 +0000

    v0.18: Added neutralisation and SDF export of an example CSD molecule

---
+++ utils/molecule_processing.py
@@ -0,0 +1,15 @@
+from rdkit import Chem
+
+def neutralize_atoms(mol):
+    pattern = Chem.MolFromSmarts("[+1!h0!$([*]~[-1,-2,-3,-4]),-1!$([*]~[+1,+2,+3,+4])]")
+    at_matches = mol.GetSubstructMatches(pattern)
+    at_matches_list = [y[0] for y in at_matches]
+    if len(at_matches_list) > 0:
+        for at_idx in at_matches_list:
+            atom = mol.GetAtomWithIdx(at_idx)
+            chg = atom.GetFormalCharge()
+            hcount = atom.GetTotalNumHs()
+            atom.SetFormalCharge(0)
+            atom.SetNumExplicitHs(hcount - chg)
+            atom.UpdatePropertyCache()
+    return mol
